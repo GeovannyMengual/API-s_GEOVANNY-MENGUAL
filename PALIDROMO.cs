ff53aad540574be45c24b59ed29f0188e80de304
GeovannyMengual <91901655+GeovannyMengual@users.noreply.github.com>
2021-10-04 16:25:56 -0500

    Add files via upload

---
+++ PALIDROMO.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class PALIDROMO
+    {
+        //Aqui van mis variables para luego llamarlos al controller
+        public string P { get; set; }
+
+
+        public string R { get; set; }  
+
+
+    }
+}
